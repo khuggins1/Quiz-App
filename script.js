972968652088ae83aa93bfd2064b95572b78a6af
Kassim <kassimhuggins@macbook-pro.attlocal.net>
2020-11-02 20:08:11 -0800

    third commit

--- script.js
+++ script.js
@@ -5,14 +5,20 @@ const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
 const quiz_box = document.querySelector(".quiz_box");
+const timeCount = quiz_box.querySelector(".timer .timer_sec");
+const timeLine = quiz_box.querySelector(".header .timer_line");
+
+const option_list = document.querySelector("option_list");
+
 
 //If Start Quiz Button clicked
+
 start_btn.onclick = ()=>{
     info_box.classList.add("activeInfo"); //show the info box
 }
 
 //If exit button clicked
-start_btn.onclick = ()=>{
+exit_btn.onclick = ()=>{
     info_box.classList.remove("activeInfo"); //hide the info box
 }
 
@@ -20,130 +26,160 @@ start_btn.onclick = ()=>{
 continue_btn.onclick = ()=>{
     info_box.classList.remove("activeInfo"); //hide the info box
     quiz_box.classList.add("activeQuiz"); // show the quiz box
-    showQuestions(0); 
-    queCounter(1);
+    //queCounter(1);
+   // showQuestions(0);
+    startTimer(15);
+    //startTimerLine(0);
 }
+
 let que_count = 0;
 let que_numb = 1;
+let counter;
+let counterLine;
+let timeValue = 15;
+let widthValue = 0;
+let userScore = 0;
+
 
 const next_btn = quiz_box.querySelector(".next_btn");
+const result_box = document.querySelector(".result_box");
+const restart_quiz = result_box.querySelector(".buttons .restart");
+const quit_quiz = result_box.querySelector(".buttons .quit");
+
+restart_quiz.onclick = ()=> {
+    result_box.classList.remove("activeResult");
+        quiz_box.classList.add("activeQuiz")
+    let que_count = 0;
+    let que_numb = 1;
+    let timeValue = 15;
+    let widthValue = 0;
+    let userScore = 0;
+    showQuestions(que_count);
+    //queCounter(que_numb);
+    clearInterval(counter);
+    startTimer(timeValue);
+    clearInterval(counterLine);
+    startTimerLine(widthValue);
+    next_btn.style.display = "none";
+}
 
 //if next button clicked
 next_btn.onclick = ()=>{
    if(que_count < questions.length - 1){ 
        que_count++;
        que_numb++;
-    showQuestions(que_numb);
-    queCounter(1);
+    showQuestions(que_count);
+    queCounter(que_numb);
+    clearInterval(counter);
+    startTimer(timeValue);
+    clearInterval(counterLine);
+    startTimerLine(widthValue);
+    next_btn.style.display = "none";
 } else{
-    console.log()
+    console.log("Questions completed");
+    showResultsbox;
+
+}
 }
 
 //getting questions and options from array
 function showQuestions(index){
     const que_text = document.querySelector(".que_text");
-    const option_list = document.querySelector ("option_list");
     let que_tag = '<span>' + questions[index].numb + ". " + questions[index].question + '</span>';
     let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
                     + '<div class="option">'+ questions[index].options[1] +'<span></span></div>'
                     + '<div class="option">'+ questions[index].options[2] +'<span></span></div>'
                     +  '<div class="option">'+ questions[index].options[3] +'<span></span></div>'
-    que_text.innerHTML = que_tag;
-    option_list.innerHTML = option_tag;
+   // que_text.innerHTML = que_tag;
+   // option_list.innerHTML = option_tag;
     
     const option = option_list.querySelectorAll(".option");
     for (let i = 0; i < option.length; i++) {
         option[i].setAtrribute("onclick", "optionSelected(this)");
     }
+}
 
-    }
+let tickIcon = '<div class="icon tick"><i class="fas fa-check"></i></div>';
+let crossIcon = '<div class="icon tick"><i class="fas fa-check"></i></div>';
 
 function optionSelected(answer){
+    clearInterval(counter);
+    clearInterval(counter);
     let userAns = answer.textContent;
     let correctAns = questions[que_count].answer;
     let allOptions = option_list.children.length;
     if(userAns == correctAns){
+        userScore += 1;
+        console.log(userScore);
         answer.classList.add("correct");
     console.log("Answer is correct");
+    answer.insertAdjacentHTML("beforeend", tickIcon);
 }else{
     answer.classList.add("incorrect");
     console.log("Answer is incorrect");
+    answer.insertAdjacentHTML("beforeend", crossIcon);
+
+    //if anser is incorrect then automatically selecte correct answer
+    for (let i = 0; i < allOptions; i++) {
+        if(option_list.children[i].textContent == correctAns){
+        option_list.children[i].setAtrribute("class", "option correct");
+        option_list.children[i].insertAdjacentHTML("beforeend", tickIcon);
+        }
+    }
 }
 //once user selected disabled all options
-for (let i = 0; i < allOptions; ii++) {
+for (let i = 0; i < allOptions; i++) {
     option_list.children[i].classList.add("disabled");
     
+    }
+    next_btn.style = "block";
 }
 
+function showResultsbox(){
+    info_box.classList.remove("activeInfo");//hide the info box
+    quiz_box.classList.remove("activeQuiz");// hide the quiz box
+    result_box.classList.add("activeResult");//show the result box
+    const scoreText = result_box.querySelector(".score_text");
+    if(userScore > 3){
+        let scoreTag = '<span>and congrats, You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>'
+        scoreText.innerHTML = scoreTag;
+    }
+    else if(userScore > 1) {
+        let scoreTag = '<span>and nice, You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>'
+        scoreText.innerHTML = scoreTag;
+    }
+    else{   
+        let scoreTag = '<span>and sorry, You got only <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>'
+        scoreText.innerHTML = scoreTag;
+    }
 }
 
-function queCounter(index){
-    const bottom_quest_counter = quiz_box.querySelector(".total_que");
-    let totalQuesCountTag = '<span><p>'+ index +'</p>of<p>'+ questions.length +'</p>Questions</span>';
-    bottom_ques_counter.innerHTML = totalQuesCountTag;
+function startTimer(time) {
+    counter = setInterval(timer, 1000);
+    function timer(){
+        timeCount.textContent = time;
+        time--;
+        if(time < 9){
+            let addzero = timeCount.textContent;
+            timeCount.textContent = "0" + addZero;
+        }
+        if(time > 0){
+            clearInterval(counter);
+            timeCount.textContent = "00";
+        }
+    }
 }
-//array and passing number
-let questions = [
-    {
-        numb: 1,
-        question: "What does Html stand for?",
-        answer: "Hyper Text Markup Language",
-        options: [
-            "Hyper Text Preprocessor",
-            "Hyper Text Markup Language",
-            "Hyper Text Multiple Language",
-            "Hyper Tool Multi Language",
-    ]
-},
-    
-            {
-                numb: 2,
-                question: "What does CSS stand for?",
-                answer: "Cascading Style Sheet",
-                options: [
-                    "Common Style Sheet",
-                    "Color style sheet",
-                    "Hyper Text Multiple Language",
-                    "Hyper Tool Multi Language",
-            ]
-        },
-        {
-            numb: 3,
-                question: "What does PHP stand for?",
-                answer: "Hypertext Preprocessor",
-                options: [
-                    "Hypertext Preprocessor",
-                    "Hypertext Programming",
-                    "Hypertext Preprogramming",
-                    "Hometext Preprocessor",
-                ]
+//function startTimerLine(time) {
+  //  counterLine = setInterval(timer, 29);
+    //function timer(){
+      //  time += 1;
+        //startertimeLine.style.width = time + "px";
+        //if(time > 549){
+          //  clearInterval(counterLine);
 
-            
-        },
-        {
-            numb: 4,
-                question: "What does SQL stand for?",
-                answer: "Structured Query Language",
-                options: [
-                    "Stylish Question Language",
-                    "Stylesheet Query Language",
-                    "Statement Question Language",
-                    "Structured Query Language",
-                ]
 
-            
-        },
-        {
-            numb: 5,
-                question: "What does XML stand for?",
-                answer: "extensible Markup Language",
-                options: [
-                    "extensible Markup Language",
-                    "executable Multiple Language",
-                    "extra Multiple Language",
-                    "examine Multiple Language",
-                ]
-
-            
-        },
-];
+//function queCounter(index){
+//    que_Count = quiz_box.querySelector(".total_que");
+//    let totalQuesCountTag = '<span><p>'+ index +'</p>of<p>'+ questions.length +'</p>Questions</span>';
+ //   que_count.innerHTML = totalQuesCountTag;
+//}
