a8468f5ba34692f435029042027d8e9689be29ad
Kassim <kassimhuggins@macbook-pro.attlocal.net>
2020-12-23 19:00:42 -0800

    fix questions

--- Assets/questions.js
+++ Assets/questions.js
@@ -4,59 +4,59 @@ let questions = [
         question: "What does CSS stand for?",
         answer: "Cascading Style Sheets",
         options: [
-            "Hyper Text Preprocessor",
-            "Hyper Text Markup Language",
-            "Hyper Text Multiple Language",
-            "Hyper Tool Multi Language",
+            "Cascading Style Sheets",
+            "Cascading Style Smiles",
+            "Cascading Super Style",
+            "Cascading Style Sass",
     ]
 },
     
             {
                 numb: 2,
-                question: "What does CSS stand for?",
-                answer: "Cascading Style Sheet",
+                question: "What are the steps to push to github?",
+                answer: "add, commit, and push",
                 options: [
-                    "Common Style Sheet",
-                    "Color style sheet",
-                    "Hyper Text Multiple Language",
-                    "Hyper Tool Multi Language",
+                    "commit, push, and add",
+                    "add, commit, and push",
+                    "push, add, and commit ",
+                    "push, add, and commit",
             ]
         },
         {
             numb: 3,
-                question: "What does PHP stand for?",
-                answer: "Hypertext Preprocessor",
+                question: "What does API stand for?",
+                answer: "Application Programming Interface",
                 options: [
-                    "Hypertext Preprocessor",
-                    "Hypertext Programming",
-                    "Hypertext Preprogramming",
-                    "Hometext Preprocessor",
+                    " ",
+                    "Always Programming Interface",
+                    "Application Programe Internet",
+                    "Application Programming Interface",
                 ]
 
             
         },
         {
             numb: 4,
-                question: "What does SQL stand for?",
-                answer: "Structured Query Language",
+                question: "What does Sass stand for?",
+                answer: "Syntactically Awesome Style Sheets",
                 options: [
-                    "Stylish Question Language",
-                    "Stylesheet Query Language",
-                    "Statement Question Language",
-                    "Structured Query Language",
+                    "Stylish Awesome style sheets",
+                    "Syntactically Applications style sheets",
+                    "Syntactically Awesome Style Sheets",
+                    "Styling apps solo sheets",
                 ]
 
             
         },
         {
             numb: 5,
-                question: "What does XML stand for?",
-                answer: "extensible Markup Language",
+                question: "Who is the best developer?",
+                answer: "Kassim",
                 options: [
-                    "extensible Markup Language",
-                    "executable Multiple Language",
-                    "extra Multiple Language",
-                    "examine Multiple Language",
+                    "Bill Gates",
+                    "Elon Musk",
+                    "Kassim",
+                    "Mark Zuckerburg",
                 ]
 
             
